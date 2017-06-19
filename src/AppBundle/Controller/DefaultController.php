ba4863c4b00035aa8744cd3874827307baf1ac50
Romain Richard <rrichard.external@lafourchette.com>
2017-06-19 17:14:35 +0200

    add pic template

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -26,8 +26,6 @@ class DefaultController extends Controller
     public function picsAction(Request $request)
     {
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-        ]);
+        return $this->render('default/pics.html.twig');
     }
 }
